e27a713fdeeccb744018b8aebd96b047cc71b158
zandilemaleka <malekabaatile@gmail.com>
2025-10-24 19:46:12 +0200

    Added dynamic date display

--- java.js
+++ java.js
@@ -14,8 +14,14 @@ hours = hours.toString().padStart(2, '0');
 minutes = minutes.toString().padStart(2, '0');
 seconds = seconds.toString().padStart(2, '0');
 
+//Date display
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const dateString = now.toLocaleDateString(undefined, options);
+
+
 document.getElementById('time').textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
 document.getElementById('ampm').textContent = ampm;
+document.getElementById('date').textContent = dateString;
 }
 UpdateClock();
 setInterval(UpdateClock, 1000);
