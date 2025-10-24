dc1720f96b7b7a3424c03bd39add8d5631bee29a
zandilemaleka <malekabaatile@gmail.com>
2025-10-24 15:41:39 +0200

    Added 24 hour conversion

--- java.js
+++ java.js
@@ -3,5 +3,10 @@ const now = new Date();
 let hours = now.getHours();
 let minutes = now.getMinutes();
 let seconds = now.getSeconds();
+
+
+//convert to 24 hour format
+let ampm = hours >= 12 ? 'PM' : 'AM';
+hours = hours % 12 || 12;
 }
 UpdateClock();
